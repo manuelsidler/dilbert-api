ad81e9340aee7b4e22df25f0c2ce12c8d547e881
Manuel Sidler <manuel.sidler@swisscom.com>
2019-11-21 11:05:23 +0100

    fetch dilbert comics (scratch)

---
+++ src/Scratchpad.fsx
@@ -0,0 +1,38 @@
+#r "../packages/FSharp.Data/lib/netstandard2.0/FSharp.Data.dll"
+
+open FSharp.Data
+
+type DilbertComic = 
+    { Title : string
+      ImageUrl : string  
+      Tags : string list }
+
+type SearchComic = string -> DilbertComic list
+
+let getComicContainers (doc:HtmlDocument) = doc.CssSelect("div.img-comic-container")
+    
+let getImageElement (comicContainer:HtmlNode) =
+    comicContainer.CssSelect("div.img-comic-container > a.img-comic-link > img.img-comic")
+    |> List.head
+
+let getImageAttribute attributeName (imageElement:HtmlNode) = 
+    imageElement.AttributeValue(attributeName)
+
+let getTitle = getImageElement >> getImageAttribute "alt"
+
+let getImage = getImageElement >> getImageAttribute "src"
+
+let getTags (comicContainer:HtmlNode) =
+    comicContainer.CssSelect("div.meta-info-container > p.comic-tags > a.link")
+    |> List.map (fun x -> x.InnerText())
+
+let searchComic : SearchComic =
+    fun tag ->
+        sprintf "https://dilbert.com/search_results?terms=%s" tag
+        |> HtmlDocument.Load
+        |> getComicContainers
+        |> List.map (fun comicContainer -> { Title = comicContainer |> getTitle
+                                             ImageUrl = comicContainer |> getImage
+                                             Tags = comicContainer |> getTags } )
+
+let comics = searchComic "Boss"
